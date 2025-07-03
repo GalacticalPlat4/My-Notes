956cb9f18c1191e24300fad30b03a17352f14e41
Chris Garcia <chris@vyxen.art>
2025-07-02 22:02:43 -0400

    vault backup: 2025-07-02 22:02:42

---
+++ Creating a PSDrive.ps1
@@ -0,0 +1,8 @@
+﻿# This is creating a new PowerShell drive that only exist in PowerShell Example: folder called PSforWork lives in the Demo folder in Root.
+new-psdrive -name PSforWork -Description "This is a test location for my PS scripts for work" -PSProvider FileSystem -Root C:\Demo
+
+# First go to Root
+cd c:\
+
+# Then type the PSDrive (PowerShell drive)
+cd psforwork:
